739bb04b046b4b73e578fcb422a086bc2b819d62
Adrian Nawalinski <adrian.nawalinski@gmail.com>
2024-02-07 15:18:12 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,30 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor() {
+  }
+  todos = [
+    { task: 'Zrobić zakupy', completed: false },
+    { task: 'Umyć samochód', completed: true },
+    { task: 'Przygotować obiad', completed: false },
+  ];
+
+  newTask: string = '';
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.todos.push({ task: this.newTask, completed: false });
+      this.newTask = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  toggleCompletion(index: number) {
+    this.todos[index].completed = !this.todos[index].completed;
+  }
+
 
 }
